ffa2ce0104481640a56b81026e16454c74776093
lcparker <lachlan.parker@protonmail.com>
2021-04-17 21:56:02 +1000

    add z-buffer funcionality

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green   = TGAColor(0, 255,   0,  255);
 const TGAColor blue   = TGAColor(0, 0,   255,  255);
 Model *model = NULL;
-const int width  = 800;
-const int height = 500;
+const int width  = 1000;
+const int height = 1000;
 
 void line(Vec2i v0, Vec2i v1, TGAImage &image, TGAColor color) {
 /* Draw a line by coloring the pixels between two points. */
@@ -44,15 +44,6 @@ void line(Vec2i v0, Vec2i v1, TGAImage &image, TGAColor color) {
 	}
 }
 
-Vec3f barycentric(Vec2i *pts, Vec2i P){
-	Vec3f xs(pts[2].x-pts[0].x, pts[1].x-pts[0].x,pts[0].x-P.x);
-	Vec3f ys(pts[2].y-pts[0].y, pts[1].y-pts[0].y,pts[0].y-P.y);
-	Vec3f u = xs ^ ys;
-	
-	if(std::abs(u.z) <1) return Vec3f(-1,1,1); // triangle is "degenerate", just return negative
-	return Vec3f(u.x/u.z,u.y/u.z,1.-(u.x+u.y)/u.z);
-}
-
 void rasterize(Vec2i v0, Vec2i v1, int ybuffer[width], TGAImage &image, TGAColor color){
 	// Draw a line, but only if it's closer to the camera than what's already
 	// been drawn.
@@ -69,7 +60,16 @@ void rasterize(Vec2i v0, Vec2i v1, int ybuffer[width], TGAImage &image, TGAColor
 	}
 }
 
-void triangle_bary(Vec2i *v, TGAImage &image, TGAColor color) {
+Vec3f barycentric(Vec2i *pts, Vec2i P){
+	Vec3f xs(pts[2].x-pts[0].x, pts[1].x-pts[0].x,pts[0].x-P.x);
+	Vec3f ys(pts[2].y-pts[0].y, pts[1].y-pts[0].y,pts[0].y-P.y);
+	Vec3f u = xs ^ ys;
+	
+	if(std::abs(u.z) <1) return Vec3f(-1,1,1); // triangle is "degenerate", just return negative
+	return Vec3f(u.x/u.z,u.y/u.z,1.-(u.x+u.y)/u.z);
+}
+
+void triangle_bary(Vec3f *v, float zbuffer[width][height], TGAImage &image, TGAColor color) {
 	/* Using barycentric coordinates to draw the triangle, which
 	 * allows us to exploit parallelisation, even though on a 
 	 * single thread it will take longer. 
@@ -78,16 +78,26 @@ void triangle_bary(Vec2i *v, TGAImage &image, TGAColor color) {
 	// Upper left and lower right point of triangle	bounding box
 
 	// whether these are int or float doesn't seem to affect performance.
-	Vec2i ur = Vec2i(std::max({v[0].x,v[1].x,v[2].x}),std::max({v[0].y,v[1].y,v[2].y}));
-	Vec2i ll = Vec2i(std::min({v[0].x,v[1].x,v[2].x}),std::min({v[0].y,v[1].y,v[2].y}));
+	Vec2f ur = Vec2f(std::max({v[0].x,v[1].x,v[2].x}),std::max({v[0].y,v[1].y,v[2].y})); //upper right corner of bouding box
+	Vec2f ll = Vec2f(std::min({v[0].x,v[1].x,v[2].x}),std::min({v[0].y,v[1].y,v[2].y})); //lower left corner
 
-	for (int x = ll.x; x<=ur.x;x++){
-		for (int y = ll.y; y<=ur.y;y++){
-			Vec3f comps  = barycentric(v,Vec2i(x,y));
-			// neat trick: if cancels as soon as it can verify truth,
-			// so it'll be faster to use || than &&
+	/*
+	for(int i=0;i<3;i++){
+		std::cout << v[i] << std::endl;
+	}
+	*/
+	for (int x = (ll.x+1.) * width/2.; x<=(ur.x+1.)*width/2.;x++){
+		for (int y = (ll.y+1.)*height/2; y<=(ur.y+1.)*height/2.;y++){
+			Vec2i pts[3] = {Vec2i((v[0].x+1.)*width/2,(v[0].y+1.)*height/2),
+			   Vec2i((v[1].x+1.)*width/2,(v[1].y+1.)*height/2),
+			   Vec2i((v[2].x+1.)*width/2,(v[2].y+1.)*height/2)};
+			Vec3f comps  = barycentric(pts,Vec2i(x,y));
 			if(comps.x < 0 || comps.y < 0 || comps.z < 0) continue;
-		   	image.set(x,y,color);
+			float z = (v[0].z)*comps.x + (v[1].z)*comps.y + (v[2].z)*comps.z;
+			if(zbuffer[x][y] < z){
+				zbuffer[x][y] = z;
+		   		image.set(x,y,color);
+			}
 		}
 	}
 }
@@ -105,14 +115,11 @@ void triangle_mono(Vec2i *v, TGAImage &image, TGAColor color) {
 	int ya;
 	int yb;
 	
-	std::cout << "drawing" << std::endl;
-
 	for(int x = v[0].x; x<v[1].x;x++){
 		float t = (x-v[0].x)/(float)(v[2].x-v[0].x);
 		float s  = (x-v[0].x)/(float)(v[1].x-v[0].x);
 		ya = (1.-t)*v[0].y + t*(v[2].y);		
 		yb = (1.-s)*v[0].y + s*(v[1].y);
-		std::cout << "x = " << x << std::endl;
 		if(ya < yb) std::swap(ya,yb);	
 		for(int y=yb;y<ya;y++){
 		   	image.set(x,y,color);
@@ -123,7 +130,6 @@ void triangle_mono(Vec2i *v, TGAImage &image, TGAColor color) {
 		float s = (v[2].x-x)/(float)(v[2].x-v[1].x);
 		ya = (1.-s)*v[2].y + s*v[1].y;		
 		yb = (1.-t)*v[2].y + t*v[0].y;
-		std::cout << "x = " << x << std::endl;
 		if(ya < yb) std::swap(ya,yb);	
 		for(int y=yb;y<ya;y++){
 		   	image.set(x,y,color);
@@ -133,23 +139,24 @@ void triangle_mono(Vec2i *v, TGAImage &image, TGAColor color) {
 
 void drawfacemesh(const char *filename, TGAImage &image){
 	model = new Model(filename);
+	float zbuffer[width][height];
+	for(int i=0;i<width;i++){
+		for(int j=0;j<height;j++){
+			zbuffer[i][j] = -2;
+		}
+	}
 	for(int i=0; i<model->nfaces();i++){
 		std::vector<int> face = model->face(i);
-		Vec2i pts[3];
-		Vec3f obj_coords[3]; // the coords as given in the obj file
+		Vec3f pts[3];
 		for (int j=0; j<3; j++){
-			Vec3f v = model->vert(face[j]); 
-			// obj file stores as "v a b c" where -1<= a,b,c<=1,
-			// so need to rescale
-			pts[j] = Vec2i((v.x+1.)*width/2.,(v.y+1.)*height/2.);
-			obj_coords[j] = v;
+			pts[j] = (model->vert(face[j]));
 		}	
-		Vec3f normal = (obj_coords[2]-obj_coords[0])^(obj_coords[1]-obj_coords[0]);
+		Vec3f normal = (pts[2]-pts[0])^(pts[1]-pts[0]);
 		normal.normalize();
 		Vec3f light_dirn = Vec3f(0,0,-1); // light goes in the -z dir'n
 		float brightness = normal*light_dirn;
 		if (brightness > 0){
-			triangle_bary(pts,image,TGAColor(brightness*255,brightness*255,brightness*255,255));
+			triangle_bary(pts,zbuffer,image,TGAColor(brightness*255,brightness*255,brightness*255,255));
 		}
 	}
 	delete model;
@@ -161,12 +168,22 @@ int main(int argc, char** argv) {
 
 	TGAImage image(width, height, TGAImage::RGB);
 	filename = "obj/african_head.obj";
-	// drawfacemesh(filename, image); // Draw a facemesh
+	drawfacemesh(filename, image); // Draw a facemesh
 
-	int ybuffer[width] = {0};
-	rasterize(Vec2i(20, 34), Vec2i(744, 400), ybuffer, image, red);
-	rasterize(Vec2i(120, 434), Vec2i(444, 400), ybuffer, image, green);
-	rasterize(Vec2i(330, 463), Vec2i(594, 200), ybuffer, image, blue);
+//   Vec3f pts[3] = {Vec3f(0.3,0.2,0), Vec3f(-0.1,-0.5, 0), Vec3f(0.7, 1, 0)}; 	
+//	float zbuffer[width][height];
+//	for (int i = 0;i<width;i++){
+//		for(int j=0;j<height;j++){
+//			zbuffer[i][j] = -2;
+//		}
+//	}
+//	triangle_bary(pts, zbuffer , image, red);
+
+	// this gives reasonable answers
+// 	Vec3f t0[3] = {Vec3f(10, 70,0),   Vec3f(50, 160,0),  Vec3f(70, 80, 0)}; 
+//	Vec3f b = barycentric(t0, Vec2i(20,90));
+//	std::cout << b << std::endl;
+
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
